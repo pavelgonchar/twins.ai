ee3c54936fe4f8dc99cdc6a7d1dfbb99c7344d12
pavelgonchar <pavel.gonchar@gmail.com>
2016-06-18 19:05:58 +0300

    coming soon page

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(function() {
+
+	$(".lever_container button").mouseup(function(event){
+		$(this).removeClass('activate');
+	});
+
+	$('.lever_container button').mousedown(function() {
+		$(this).addClass('activate');
+		$('#slot1').carousel({
+			interval: 222,
+			wrap: false,
+		}).carousel(0);
+		$('#slot2').carousel({
+			interval: 189,
+			wrap: false,
+		}).carousel(0);
+		$('#slot3').carousel({
+			interval: 135,
+			wrap: false,
+		}).carousel(0);
+
+		setTimeout("$('.lever_container button').mouseup();", 1500);
+		
+	});
+	$('.lever_container button').mousedown();
+
+});
